82696a4b856c99c615b887c70c123d03046dacda
edwardivanov <49359675+edwardivanov@users.noreply.github.com>
2020-01-12 21:09:35 +0200

    Initial code added

---
+++ TestGtHub/TestGtHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGtHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Azis!");
+        }
+    }
+}
